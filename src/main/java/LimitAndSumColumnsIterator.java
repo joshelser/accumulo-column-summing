83109e8c8dd443983ca0e6f86cb4791636362eaa
Josh Elser <josh.elser@gmail.com>
2014-03-23 22:01:49 -0400

    Get a basic test working.

---
+++ src/main/java/LimitAndSumColumnsIterator.java
@@ -0,0 +1,304 @@
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.apache.accumulo.core.client.IteratorSetting;
+import org.apache.accumulo.core.data.ByteSequence;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.PartialKey;
+import org.apache.accumulo.core.data.Range;
+import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.iterators.IteratorEnvironment;
+import org.apache.accumulo.core.iterators.LongCombiner;
+import org.apache.accumulo.core.iterators.LongCombiner.Type;
+import org.apache.accumulo.core.iterators.SortedKeyValueIterator;
+import org.apache.accumulo.core.iterators.TypedValueCombiner.Encoder;
+import org.apache.accumulo.core.iterators.WrappingIterator;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.io.Text;
+import org.apache.log4j.Logger;
+
+import com.google.common.base.Preconditions;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * 
+ */
+public class LimitAndSumColumnsIterator extends WrappingIterator {
+  private static final Logger log = Logger.getLogger(LimitAndSumColumnsIterator.class);
+  
+  public static final String COLUMNS = "limit.columns";
+  public static final String TYPE = "limit.type";
+
+  public static void setColumns(IteratorSetting cfg, Collection<String> columns) {
+    Preconditions.checkNotNull(columns);
+    final StringBuilder sb = new StringBuilder(64);
+    for (String column : columns) {
+      if (0 != sb.length()) {
+        sb.append(',');
+      }
+      sb.append(column);
+    }
+    cfg.addOption(COLUMNS, sb.toString());
+  }
+
+  private SortedSet<Text> getColumns(Map<String,String> options) {
+    String serializedColumns = options.get(COLUMNS);
+    if (null == serializedColumns) {
+      throw new IllegalArgumentException("Did not find " + COLUMNS + " in the options map");
+    }
+
+    String[] columns = StringUtils.split(serializedColumns, ',');
+    TreeSet<Text> sortedColumns = new TreeSet<Text>();
+    for (String column : columns) {
+      sortedColumns.add(new Text(column));
+    }
+
+    return sortedColumns;
+  }
+  
+  private void setEncoder(Map<String,String> options) {
+    String type = options.get(TYPE);
+    if (type == null)
+      throw new IllegalArgumentException("no type specified");
+    switch (Type.valueOf(type)) {
+      case VARLEN:
+        setEncoder(LongCombiner.VAR_LEN_ENCODER);
+        return;
+      case FIXEDLEN:
+        setEncoder(LongCombiner.FIXED_LEN_ENCODER);
+        return;
+      case STRING:
+        setEncoder(LongCombiner.STRING_ENCODER);
+        return;
+      default:
+        throw new IllegalArgumentException();
+    }
+  }  
+
+  /**
+   * Sets the Encoder<V> used to translate Values to V and back.
+   * 
+   * @param encoder
+   */
+  protected void setEncoder(Encoder<Long> encoder) {
+    this.encoder = encoder;
+  }
+
+  /**
+   * A convenience method for setting the long encoding type.
+   * 
+   * @param is
+   *          IteratorSetting object to configure.
+   * @param type
+   *          LongCombiner.Type specifying the encoding type.
+   */
+  public static void setEncodingType(IteratorSetting is, LongCombiner.Type type) {
+    is.addOption(TYPE, type.toString());
+  }
+
+  private Encoder<Long> encoder = null;
+  private SortedSet<Text> desiredColumns;
+  private Long sum;
+
+  private Text rowHolder, colfamHolder;
+  private Key topKey;
+  private Value topValue;
+
+  // Internal state used during seek() or next()
+  private Range currentRange;
+  private Collection<ByteSequence> currentColumnFamilies;
+  private boolean currentColumnFamiliesInclusive;
+  
+  public LimitAndSumColumnsIterator() {
+    this.rowHolder = new Text();
+    this.colfamHolder = new Text();
+    this.sum = 0l;
+  }
+  
+  @Override
+  public SortedKeyValueIterator<Key,Value> deepCopy(IteratorEnvironment env) {
+    LimitAndSumColumnsIterator skvi = new LimitAndSumColumnsIterator();
+    skvi.desiredColumns = this.desiredColumns;
+    skvi.encoder = this.encoder;
+    
+    // These are likely to not be used (reseeked immediately after deepCopy), but it's prudent to copy them anyways
+    skvi.topKey = this.topKey;
+    skvi.sum = this.sum;
+    skvi.topValue = this.topValue;
+    
+    return skvi;
+  }
+
+  @Override
+  public void init(SortedKeyValueIterator<Key,Value> source, Map<String,String> options, IteratorEnvironment env) throws IOException {
+    super.init(source, options, env);
+    desiredColumns = getColumns(options);
+    setEncoder(options);
+  }
+
+  @Override
+  public void seek(Range range, Collection<ByteSequence> columnFamilies, boolean inclusive) throws IOException {
+    // Make sure we invalidate our last record
+    nextRecordNotFound();
+    
+    log.debug("Seeking to " + range);
+
+    getSource().seek(range, columnFamilies, inclusive);
+    currentRange = range;
+    currentColumnFamilies = columnFamilies;
+    currentColumnFamiliesInclusive = inclusive;
+    aggregate();
+  }
+
+  @Override
+  public void next() throws IOException {
+    // Make sure we invalidate our last record
+    nextRecordNotFound();
+
+    // Catch the case where we have no data and there's nothing to next()
+    if (!getSource().hasTop()) {
+      return;
+    }
+
+    getSource().next();
+    aggregate();
+  }
+
+  @Override
+  public Key getTopKey() {
+    return topKey;
+  }
+
+  @Override
+  public Value getTopValue() {
+    return topValue;
+  }
+
+  @Override
+  public boolean hasTop() {
+    // topKey and sum should be held in step -- they are either both null, or both non-null
+    if ((null == topKey && null != sum) || (null != topKey && null == sum)) {
+      throw new IllegalStateException("topKey and sum in differing states: " + topKey + " => " + sum);
+    }
+    // thus, we can just look at one to determine if there's a record to return.
+    return topKey != null;
+  }
+
+  /**
+   * Given the current position in the {@link source}, filter to only the columns specified. Sets topKey and topValue to non-null on success
+   * 
+   * @throws IOException
+   */
+  protected void aggregate() throws IOException {
+    if (!getSource().hasTop()) {
+      nextRecordNotFound();
+      return;
+    }
+    
+    while (getSource().hasTop()) {
+      final Key currentKey = getSource().getTopKey();
+      final Value currentValue = getSource().getTopValue();
+
+      currentKey.getColumnFamily(colfamHolder);
+      final Iterator<Text> remainingColumns = desiredColumns.tailSet(colfamHolder).iterator();
+      
+      if (desiredColumns.contains(colfamHolder)) {
+        // found a column we wanted
+        nextRecordFound(currentKey, currentValue);
+
+        // The tailSet returns elements greater than or equal to the provided element
+        // need to consume the equality element
+        remainingColumns.next();
+      }
+      
+      Range newRange;
+      if (remainingColumns.hasNext()) {
+        // Get the row avoiding a new Text
+        currentKey.getRow(rowHolder);
+        
+        Text nextColumn = remainingColumns.next();
+        Key nextColumnKey = new Key(rowHolder, nextColumn);
+        newRange = new Range(nextColumnKey, true, currentRange.getEndKey(), currentRange.isEndKeyInclusive());
+      } else {
+        Key nextRow = currentKey.followingKey(PartialKey.ROW);
+
+        // No more data to read, outside of where we wanted to look
+        if (!currentRange.contains(nextRow)) {
+          setReturnValue();
+          return;
+        } else {
+          // Our new range starts, at earliest, the next possible row and first column family in our desired set
+          // but still ends at the original ending
+          newRange = new Range(nextRow, true, currentRange.getEndKey(), currentRange.isEndKeyInclusive());
+        }
+      }
+      
+      log.debug("Seeking to " + newRange);
+
+      // Move down to the next Key
+      getSource().seek(newRange, currentColumnFamilies, currentColumnFamiliesInclusive);
+    }
+    
+    setReturnValue();
+  }
+
+  private void nextRecordNotFound() {
+    topKey = null;
+    sum = null;
+  }
+
+  private void nextRecordFound(Key k, Value v) {
+    if (null == topKey) {
+      topKey = k;
+    }
+    
+    if (null == sum) {
+      sum = 0l;
+    }
+    
+//    StringBuilder sb = new StringBuilder(64);
+//    sb.append("Adding to sum '");
+//    sb.append(k);
+//    sb.append("' = '");
+//    byte[] bytes = v.get();
+//    for (int i = 0; i < bytes.length; i++) {
+//      int c = 0xff & bytes[i];
+//      if (c >= 32 && c <= 126)
+//        sb.append((char) c);
+//      else
+//        sb.append("%" + String.format("%02x;", c));
+//    }
+//    sb.append("'");
+//    log.debug(sb.toString());
+//    log.debug("sum = " + sum);
+    sum += encoder.decode(v.get());
+  }
+  
+  private void setReturnValue() {
+    if (null != sum) {
+      topValue = new Value(encoder.encode(sum));
+    } else {
+      topValue = new Value(encoder.encode(0l));
+    }
+  }
+}
--- src/test/java/SumColumnsTest.java
+++ src/test/java/SumColumnsTest.java
@@ -1,23 +1,36 @@
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map.Entry;
 import java.util.Random;
 import java.util.TreeSet;
 
+import org.apache.accumulo.core.client.BatchScanner;
 import org.apache.accumulo.core.client.BatchWriter;
 import org.apache.accumulo.core.client.BatchWriterConfig;
 import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.IteratorSetting;
+import org.apache.accumulo.core.client.Scanner;
+import org.apache.accumulo.core.client.TableNotFoundException;
 import org.apache.accumulo.core.client.ZooKeeperInstance;
 import org.apache.accumulo.core.client.admin.TableOperations;
 import org.apache.accumulo.core.client.security.tokens.PasswordToken;
+import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Mutation;
-import org.apache.accumulo.core.iterators.user.SummingCombiner;
+import org.apache.accumulo.core.data.Range;
+import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.iterators.LongCombiner;
+import org.apache.accumulo.core.iterators.TypedValueCombiner.Encoder;
+import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.minicluster.MiniAccumuloCluster;
 import org.apache.accumulo.minicluster.MiniAccumuloConfig;
+import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableUtils;
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -48,17 +61,23 @@ import org.junit.rules.TestName;
  * 
  */
 public class SumColumnsTest {
+  private static final Logger log = Logger.getLogger(SumColumnsTest.class);
   private static final String PASSWD = "password";
   
   @Rule
   public TestName testName = new TestName();
   private String table;
+  private ZooKeeperInstance inst;
+  private Connector conn;
   
   private static MiniAccumuloCluster cluster;
 
   @BeforeClass
   public static void setup() throws Exception {
     File macDir = new File(System.getProperty("user.dir") + "/target/SumColumnsTest");
+    if (macDir.exists() && macDir.isDirectory()) {
+      FileUtils.deleteQuietly(macDir);
+    }
     Assert.assertTrue(macDir.mkdirs());
     MiniAccumuloConfig cfg = new MiniAccumuloConfig(macDir, PASSWD);
     cfg.setNumTservers(2);
@@ -68,26 +87,19 @@ public class SumColumnsTest {
 
   @AfterClass
   public static void stop() throws Exception {
-    cluster.stop();
-  }
-  
-  public Connector getConnector() throws Exception {
-    return (new ZooKeeperInstance(cluster.getInstanceName(), cluster.getZooKeepers())).getConnector("root", new PasswordToken(PASSWD));
+    if (null != cluster) {
+      cluster.stop();
+    }
   }
   
   @Before
-  public void setupTableForTest() throws Exception {
-    table = testName.getMethodName();
-    TableOperations tops = getConnector().tableOperations();
-    tops.create(table);
+  public void setupTest() throws Exception {
+    inst = new ZooKeeperInstance(cluster.getInstanceName(), cluster.getZooKeepers());
+    conn = inst.getConnector("root", new PasswordToken(PASSWD));
 
-    // Create our iterator to limit the columns server-side
-    IteratorSetting setting = new IteratorSetting(50, "limit", LimitColumnsIterator.class);
-    tops.attachIterator(table, setting);
-    
-    // Combine them all together
-    IteratorSetting combiner = new IteratorSetting(100, "combiner", SummingCombiner.class);
-    tops.attachIterator(table, combiner);
+    table = testName.getMethodName();
+    TableOperations tops = conn.tableOperations();
+    tops.create(table);
     
     TreeSet<Text> splits = new TreeSet<Text>();
     for (int i = 0; i < 20; i++) {
@@ -96,17 +108,36 @@ public class SumColumnsTest {
     tops.addSplits(table, splits);
   }
   
+  public BatchWriter getBatchWriter() throws Exception {
+    return conn.createBatchWriter(table, new BatchWriterConfig());
+  }
+  
   @After
   public void removeTable() throws Exception {
-    getConnector().tableOperations().delete(table);
+    try {
+      conn.tableOperations().delete(table);
+    } catch (TableNotFoundException tnfe) {
+      
+    }
   }
+  
+  private BatchScanner getBatchScanner(Authorizations auths, int numThreads, Collection<String> columns) throws Exception {
+    final BatchScanner bs = conn.createBatchScanner(table, new Authorizations(), numThreads);
 
+    // Create our iterator to limit the columns server-side
+    IteratorSetting setting = new IteratorSetting(50, "limit", LimitAndSumColumnsIterator.class);
+    LimitAndSumColumnsIterator.setColumns(setting, columns);
+    LimitAndSumColumnsIterator.setEncodingType(setting, LongCombiner.Type.VARLEN);
+    bs.addScanIterator(setting);
+    
+    return bs;
+  }
+  
   @Test
   public void test() throws Exception {
-    Random r = new Random();
+    final Encoder<Long> encoder = LongCombiner.VAR_LEN_ENCODER;
+    final Random r = new Random();
 
-    Connector conn = getConnector();
-    
     ArrayList<String> extraColumns = new ArrayList<String>();
     extraColumns.add("foo1");
     extraColumns.add("bar1");
@@ -128,16 +159,32 @@ public class SumColumnsTest {
         m.put(extraColumn, "", extraColumn);
       }
       
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      
-      WritableUtils.writeVLong(new DataOutputStream(baos), countInRow);
-      m.put(countColumn, "", baos.toString());
+      byte[] bytes = encoder.encode(new Long(countInRow));
+      m.put(countColumn, "", new Value(bytes));
       
       bw.addMutation(m);
     }
     
     bw.close();
     
+    Scanner s = conn.createScanner(table, new Authorizations());
+    for (Entry<Key,Value> entry : s) {
+      log.info(entry);
+    }
+
+    BatchScanner bs = getBatchScanner(new Authorizations(), 2, Collections.singleton(countColumn));
+    bs.setRanges(Collections.singleton(new Range()));
+    long observedSum = 0;
+    try {
+      for (Entry<Key,Value> result : bs) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(result.getValue().get());
+        observedSum += WritableUtils.readVLong(new DataInputStream(bais));
+      }
+    } finally {
+      bs.close();
+    }
+    
+    Assert.assertEquals(sum, observedSum);
   }
 
 }
